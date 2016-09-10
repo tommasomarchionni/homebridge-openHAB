d9c800ec8f7d99e9b039c9e0707baf998f2f69b3
stephan-01010011 <titz.stephan@gmail.com>
2016-09-10 11:35:51 +0200

    Fixed inconsistencies that leads to wrong brightness states.

--- items/DimmerItem.js
+++ items/DimmerItem.js
@@ -1,96 +1,64 @@
 "use strict";
 
 var request = require("request");
+var Promise = require("promise");
 
-var DimmerItem = function(widget,platform,homebridge) {
-    DimmerItem.super_.call(this, widget,platform,homebridge);
+var DimmerItem = function (widget, platform, homebridge) {
+    DimmerItem.super_.call(this, widget, platform, homebridge);
 };
 
-DimmerItem.prototype.getServices = function() {
+DimmerItem.prototype.getServices = function () {
 
     this.initListener();
-    this.setInitialState = true;
 
     this.informationService = this.getInformationServices();
 
     this.otherService = new this.homebridge.hap.Service.Lightbulb();
-    this.otherService.getCharacteristic(this.homebridge.hap.Characteristic.On)
-        .on('set', this.setItem.bind(this))
-        .on('get', this.getItemPowerState.bind(this))
-        .setValue(+this.state > 0);
 
-    this.setInitialState = true;
+    this.otherService.getCharacteristic(this.homebridge.hap.Characteristic.On)
+        .on('set', this.setOpenHabPowerState.bind(this))
+        .on('get', this.getOpenHabPowerState.bind(this))
+        .setValue(this.state > 0, function () {}, "init");
 
     this.otherService.addCharacteristic(this.homebridge.hap.Characteristic.Brightness)
-        .on('set', this.setItem.bind(this))
-        .on('get', this.getItemBrightnessState.bind(this))
-        .setValue(+this.state);
+        .on('set', this.setOpenHabBrightnessState.bind(this))
+        .on('get', this.getOpenHabBrightnessState.bind(this))
+        .setValue(+this.state, function () {}, "init");
 
     return [this.informationService, this.otherService];
 };
 
-DimmerItem.prototype.updateCharacteristics = function(message) {
+DimmerItem.prototype.updateCharacteristics = function (message) {
 
-    this.setFromOpenHAB = true;
     var brightness = +message;
-    var steps = 2;
-    if (brightness >= 0) {
-        this.otherService.getCharacteristic(this.homebridge.hap.Characteristic.Brightness)
-            .setValue(brightness,
-                function() {
-                    steps--;
-                    if (!steps) {
-                        this.setFromOpenHAB = false;
-                    }
-                }.bind(this));
-        this.otherService.getCharacteristic(this.homebridge.hap.Characteristic.On)
-            .setValue(brightness > 0,
-                function() {
-                    steps--;
-                    if (!steps) {
-                        this.setFromOpenHAB = false;
-                    }
-                }.bind(this));
-    }
+
+    this.otherService.getCharacteristic(this.homebridge.hap.Characteristic.Brightness)
+        .setValue(brightness, function () {
+        }, "remote");
+    this.otherService.getCharacteristic(this.homebridge.hap.Characteristic.On)
+        .setValue(brightness > 0, function () {
+        }, "remote");
+
 };
 
-DimmerItem.prototype.getItemPowerState = function(callback) {
+DimmerItem.prototype.updateOpenHabState = function (value, type, callback, context) {
 
     var self = this;
 
-    this.log("iOS - request power state from " + this.name);
-    request(this.url + '/state?type=json', function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            self.log("OpenHAB HTTP - response from " + self.name + ": " + body);
-            callback(undefined,+body > 0);
-        } else {
-            self.log("OpenHAB HTTP - error from " + self.name + ": " + error);
-        }
-    })
-};
-
-DimmerItem.prototype.setItem = function(value, callback) {
-
-    var self = this;
-
-    if (this.setInitialState) {
-        this.setInitialState = false;
-        callback();
+    if (context == "remote" || context == "init") {
+        callback(null);
         return;
     }
 
-    if (this.setFromOpenHAB) {
-        callback();
-        return;
-    }
-
-    this.log("iOS - send message to " + this.name + ": " + value);
-    var command = 0;
-    if (typeof value === 'boolean') {
-        command = value ? '100' : '0';
+    var command = "";
+    if (type === "Power") {
+        command = value ? 'ON' : 'OFF';
     } else {
         command = "" + value;
     }
+
+    this.log("iOS - send message to " + this.name + ": " + command);
+
     request.post(
         this.url,
         {
@@ -108,19 +76,57 @@ DimmerItem.prototype.setItem = function(value, callback) {
     );
 };
 
-DimmerItem.prototype.getItemBrightnessState = function(callback) {
 
-    var self = this;
+/**
+ * Method to get the OpenHab state. It uses a GET request to retrieve the state from the REST api.
+ * The callback method updateValues is called if the GET request succeeded.
+ */
+DimmerItem.prototype.getOpenHabState = function () {
 
-    this.log("iOS - request brightness state from " + this.name);
-    request(this.url + '/state?type=json', function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            self.log("OpenHAB HTTP - response from " + self.name + ": " + body);
-            callback(undefined,+body);
-        } else {
-            self.log("OpenHAB HTTP - error from " + self.name + ": " + error);
-        }
-    })
+    return new Promise(function (resolve, reject) {
+
+            request.get(
+                this.url + '/state',
+                function (error, response, body) {
+                    if (!error && response.statusCode === 200) {
+
+                        this.log("OpenHAB HTTP GET <" + this.name + "> - " + body);
+                        resolve(body);
+
+                    } else {
+                        this.log.error("OpenHAB HTTP ERROR <" + this.name + "> - " + error);
+                        reject(error);
+                    }
+                }.bind(this)
+            )
+        }.bind(this)
+    );
+};
+
+DimmerItem.prototype.getOpenHabPowerState = function (callback) {
+    this.getOpenHabState()
+        .then(function (state) {
+            let response = false;
+
+            if (state > 0) {response = true; }
+            callback(null, response)
+        });
+};
+
+DimmerItem.prototype.getOpenHabBrightnessState = function (callback) {
+    this.getOpenHabState()
+        .then(function (state) {
+            callback(null, +state)
+        });
+};
+
+DimmerItem.prototype.setOpenHabPowerState = function (value, callback, context) {
+    //this.updateOpenHabState(value, "Power", callback, context);
+    callback();
+};
+
+DimmerItem.prototype.setOpenHabBrightnessState = function (value, callback, context) {
+    this.updateOpenHabState(value, "Brightness", callback, context);
 };
 
 module.exports = DimmerItem;
