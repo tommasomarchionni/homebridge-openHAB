8d3b56ae2e00d1f5c277ac79d2816bf86114af74
Philip Kleimeyer <philip.kleimeyer@googlemail.com>
2017-01-04 21:56:25 +0100

    update for openhab2. extend shutters config

--- libs/ItemFactory.js
+++ libs/ItemFactory.js
@@ -1,6 +1,4 @@
 "use strict";
-const util = require('util')
-
 var exports = module.exports = {};
 exports.AbstractItem = require('../items/AbstractItem.js');
 exports.SwitchItem = require('../items/SwitchItem.js');
@@ -8,13 +6,16 @@ exports.LightbulbItem = require('../items/LightbulbItem.js');
 exports.FanItem = require('../items/FanItem.js');
 exports.OutletItem = require('../items/OutletItem.js');
 exports.DimmerItem = require('../items/DimmerItem.js');
-exports.RollershutterItem = require('../items/RollershutterItem.js');
+exports.RollershutterItem = require('../items/RollerShutterItem.js');
 exports.TemperatureSensorItem = require('../items/TemperatureSensorItem.js');
 exports.NumberItem = require('../items/NumberItem.js');
 exports.ContactItem = require('../items/ContactItem.js');
 exports.MotionSensorItem = require('../items/MotionSensorItem.js');
 exports.LightSensorItem = require('../items/LightSensorItem.js');
 exports.ThermostatItem = require('../items/ThermostatItem.js');
+exports.GarageDoorOpenerItem = require('../items/GarageDoorOpenerItem.js');
+exports.ColorItem = require('../items/ColorItem.js');
+
 
 exports.Factory = function(OpenHABPlatform,homebridge) {
     this.platform = OpenHABPlatform;
@@ -124,8 +125,6 @@ exports.Factory.prototype.traverseSitemap = function(jsonSitmap,factory) {
     //initialize variables
     var lastLabel="";
 	var lastTitle="";
-	//console.log("new json")
-	//console.log(util.inspect(jsonSitmap, false, null))
 	
     for (var key in jsonSitmap) {
 		
